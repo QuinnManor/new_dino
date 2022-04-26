47724d3473701fbe9aaebb65594ab124d58896dc
QuinnManor <matthewq1428@gmail.com>
2022-04-26 19:33:41 -0400

    Creating human object from input

--- app.js
+++ app.js
@@ -15,11 +15,25 @@ function human(name, weight, height, diet) {
   this.diet = diet;
 }
 
-function fetchData(){
-  fetch('dino.json')
-    .then(response => {
-      return response.json(); 
+function fetchData() {
+  fetch("dino.json")
+    .then((response) => {
+      return response.json();
     })
-    .then((data) => console.log(data))
+    .then((data) => console.log(data));
 }
-fetchData(); 
+
+function getHumanData() {
+  return (function () {
+    const humanName = document.getElementById("name").value;
+    const humanFeet = document.getElementById("feet").value;
+    const humanInches = document.getElementById("inches").value;
+    const humanWeight = document.getElementById("weight").value;
+    const humanDiet = document.getElementById("diet").value;
+    let height = parseInt(humanFeet) * 12 + parseInt(humanInches);
+
+    return new human(humanName, humanWeight, height, humanDiet);
+  })();
+}
+
+button.addEventListener("click", getHumanData);
