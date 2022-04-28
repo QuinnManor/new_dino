6a3d6527f94680d21ec96c5cd40be170cc064b10
Erica Osborn <ericaosborn@Ericas-MacBook-Air.local>
2022-04-28 18:48:37 -0400

    updated changes to correct branch

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ function dinosaur(species, weight, height, diet, where, when, fact) {
   this.when = when;
   this.fact = fact;
 }
+// human constructor
+function human (name, weight, height, diet){
+  this.name = name;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  }
+  //form reset
+  const clearScreen = (ref) => {
+  ref.remove();
+  }
+  const formRef = document.getElementById("dino-compare");
+  const compareMeButtonRef = document.getElementById("btn");
+  compareMeButtonRef.addEventListener("click",()=> clearScreen(formRef));
+  
